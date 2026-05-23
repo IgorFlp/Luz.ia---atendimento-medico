204da7e4188b89699beb11f77f7c10f194fc7cb3
IgorFlp <igor.felipe.13@hotmail.com>
2026-05-23 16:00:45 -0300

    Message Generator fully hooked

--- src/graph/nodes/messageGeneratorNode.ts
+++ src/graph/nodes/messageGeneratorNode.ts
@@ -1,16 +1,46 @@
 import type { GraphState } from '../graph.ts';
 import { AIMessage } from 'langchain';
+import { OpenRouterService } from '../../services/openRouterService.ts';
+import { getSystemPrompt, getUserPromptTemplate, MessageSchema } from '../../prompts/v1/messageGenerator.ts';
 
-export function createMessageGeneratorNode() {
-    return async (state: GraphState): Promise<GraphState> => {
+
+export function createMessageGeneratorNode(llmClient: OpenRouterService) {
+    return async (state: GraphState): Promise<Partial<GraphState>> => {
         console.log(`💬 Generating response message...`);
 
         try {
+            const hasSuccess = state.actionSuccess? "success" : "error";
+            const scenario = `${state.intent ?? 'unknown'}_${hasSuccess}`;
+            const details = {                
+                professionalName: state.professionalName,
+                patientName: state.patientName,
+                datetime: state.datetime,                
+                error: state.actionError || state.error,
+            }
+            const systemPrompt = getSystemPrompt();
+            const userPrompt = getUserPromptTemplate({ scenario, details });
 
-            return {
-                ...state,
+            const result = await llmClient.generateStructured(
+                systemPrompt,
+                userPrompt,
+                MessageSchema,
+            );
+            console.log('✅ Message generated:', result);
+            
+            if (!result.success) {
+                console.error('❌ Failed to generate message:', result.error);
+                const errorMessage = 'Desculpe, ocorreu um erro ao gerar a mensagem. Por favor, tente novamente.';
+                return {                   
+                    messages: [
+                        ...state.messages,
+                        new AIMessage(errorMessage)
+                    ],
+                };
+            }
+            return {               
                 messages: [
                     ...state.messages,
+                    new AIMessage(result.data!.message)
                 ],
             };
         } catch (error) {
